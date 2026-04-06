76bc7eade2a4357b1a212a24f640e033bef280e5
ripponmarshall-netizen <ripponmarshall@yahoo.com>
2026-04-05 22:03:18 -0500

    Add files via upload

---
+++ src/constants.ts
@@ -0,0 +1,13 @@
+export const DB_NAME = 'workoutbro_gordon_db';
+export const DB_VERSION = 1;
+
+export const APP_SHELL_CACHE = 'workoutbro-shell-v1';
+export const APP_RUNTIME_CACHE = 'workoutbro-runtime-v1';
+export const APP_OFFLINE_FALLBACK_URL = '/offline.html';
+
+export const SYNC_BATCH_LIMIT = 25;
+export const SYNC_DEFAULT_PRIORITY = 100;
+export const SYNC_MAX_RETRY_SECONDS = 7200;
+export const RECOVERY_STALE_MINUTES = 180;
+export const SCORE_VERSION = 1;
+export const DEFAULT_WEIGHT_UNIT = 'kg';
